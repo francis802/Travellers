02de235f8f7242c8f719a97b888773b10183c5b7
francis802 <franciscosccampos@gmail.com>
2023-12-09 22:23:27 +0000

    Bug Fix

--- resources/views/partials/countryButton.blade.php
+++ resources/views/partials/countryButton.blade.php
@@ -11,23 +11,3 @@
     <li><a class="dropdown-item" href="#">Banned</a></li>
   </ul>
 </div>
-
-<!-- Modal -->
-<div class="modal fade" id="subgroups-{{$group->id}}" tabindex="-1" aria-labelledby="subgroups-{{$group->id}}" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Groups</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        @foreach($group->subgroups()->get() as $subgroup)
-            @include('partials.subgroupButton', ['subgroup' => $subgroup])
-        @endforeach
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModal" data-bs-toggle="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div>
