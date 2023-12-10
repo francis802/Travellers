a3aaa3d32012ef07e03e4ed5ceab4480d7014fe8
francis802 <franciscosccampos@gmail.com>
2023-12-10 02:51:59 +0000

    More advancements in member groups

---
+++ app/Models/BannedMember.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class BannedMember extends Model
+{
+    use HasFactory;
+
+
+    public $timestamps  = false;
+
+    protected $table = 'banned_member';
+
+
+    protected $fillable = ['user_id', 'group_id'];
+
+    
+    public function user() {
+        return User::find($this->user_id);
+    }
+
+    public function group() {
+        return Group::find($this->group_id);
+    }
+
+
+}
