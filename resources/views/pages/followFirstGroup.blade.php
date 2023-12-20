2e2fe9528294080d8de99f9294da33bff463ea41
Henrique Pinheiro <heenriquepinheiro1@gmail.com>
2023-12-20 03:31:05 +0000

    Presentation of some groups to join after register

---
+++ resources/views/pages/followFirstGroup.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@include('partials.topbar')
+@include('partials.bar')
+
+@section('content')
+    @yield('bar')
+    <section id="feed">
+        @yield('topbar')
+        <section class="fst-grp-header">
+            <h2 class="fst-grp">Join your first group</h2>
+            <a href="{{ url('/home') }}" class="btn btn-primary">Skip</a>
+        </section>
+            <div id="groups-list"class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach($groups as $group)
+                        <a href="{{ url('/group/'.$group->id) }}">
+                            <div class="col">
+                                <div class="card">
+                                <img src="{{ url($group->banner_pic) }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $group->country->name }}</h5>
+                                    <p class="card-text">{{ $group->description }}</p>
+                                </div>
+                                </div>
+                            </div>
+                        </a>
+                @endforeach
+            </div>
+       
+    </section>
+@endsection
