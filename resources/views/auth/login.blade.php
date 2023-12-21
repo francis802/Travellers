c6d4e143fe854e394d994f93ad640ef78e56e1c2
Henrique Pinheiro <heenriquepinheiro1@gmail.com>
2023-12-21 11:59:30 +0000

    Fixing forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,17 +10,18 @@
     <form class="login-register-form" method="POST" action="{{ route('login') }}">
         {{ csrf_field() }}
 
-        <label for="email">E-mail</label>
-        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+        
+        <label for="email" class="form-label">Email</label>
+        <input id="email" type="email" name="email" class="form-control" value="{{ old('email') }}" required autofocus placeholder="name@example.com">
         
 
-        <label for="password" >Password</label>
-        <input id="password" type="password" name="password" required>
+        <label for="password" class="form-label">Password</label>
+        <input id="password" type="password" name="password" required class="form-control" aria-describedby="passwordHelpBlock">
         
 
         <div id="buttons-container">
-            <button class="button" type="submit">Login</button>
-            <a class="link-button" href="{{ route('register') }}">Register</a>
+            <button class="btn btn-primary" type="submit">Login</button>
+            <a class="btn btn-primary" href="{{ route('register') }}">Register</a>
         </div>
         
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,47 +9,62 @@
   <form class="login-register-form" method="POST" action="{{ route('register') }}">
       {{ csrf_field() }}
 
+
+      
       <label for="name">Name</label>
-      <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+      <input id="name" type="text" name="name" value="{{ old('name') }}" class="form-control"  required autofocus>
+     
+      <label for="username">Userame</label>
+      <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control"  required autofocus>
+      
+      <label for="country">Country</label>
+      <select id="country" name="country" class="form-select form-select-sm" aria-label="Select your country">
+        @foreach($countries as $country)
+            <option value="{{ $country->id }}">{{ $country->name }}</option>
+        @endforeach
+      </select>
+
+      <label for="email">Email</label>
+      <input id="email" type="email" name="email" class="form-control" value="{{ old('email') }}" required autofocus placeholder="name@example.com">
+
+      <label for="password">Password</label>
+      <input id="password" type="password" name="password" required class="form-control" aria-describedby="passwordHelpBlock">
+      <div id="passwordHelpBlock" class="form-text hit"> Your password must be 8-20 characters long.</div>
+      
+
+      <label for="password-confirm">Confirm Password</label>
+      <input id="password-confirm" type="password" name="password_confirmation" required class="form-control" aria-describedby="passwordHelpBlock">
+      
+
+
+    <div id="buttons-container">
+      <button class="btn btn-primary" type="submit"> Register </button>
+      <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+    </div>
+
       @if ($errors->has('name'))
         <span class="error">
             {{ $errors->first('name') }}
         </span>
       @endif
 
-      <label for="username">Username</label>
-      <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
       @if ($errors->has('username'))
         <span class="error">
             {{ $errors->first('username') }}
         </span>
       @endif
-     
-     
 
-      <label for="email">E-Mail Address</label>
-      <input id="email" type="email" name="email" value="{{ old('email') }}" required>
       @if ($errors->has('email'))
         <span class="error">
             {{ $errors->first('email') }}
         </span>
       @endif
 
-      <label for="password">Password</label>
-      <input id="password" type="password" name="password" required>
       @if ($errors->has('password'))
         <span class="error">
             {{ $errors->first('password') }}
         </span>
       @endif
-
-      <label for="password-confirm">Confirm Password</label>
-      <input id="password-confirm" type="password" name="password_confirmation" required>
-
-    <div id="buttons-container">
-      <button class="button" type="submit"> Register </button>
-      <a class="link-button" href="{{ route('login') }}">Login</a>
-    </div>
   </form>
 </section>
 @endsection
