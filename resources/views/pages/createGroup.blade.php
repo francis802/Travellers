80583904e1fd4f91c93b41a0efd3479adeb2a677
up202108735 <up202108735@fe.up.pt>
2023-12-21 12:11:37 +0000

    Somethings in groups corrected

--- resources/views/pages/createGroup.blade.php
+++ resources/views/pages/createGroup.blade.php
@@ -23,7 +23,13 @@
                                 <input type="file" id="image" name="image" accept="image/png, image/jpeg" style="display: none" onchange="displayImage(this)">
                         </section>
                         <input type="hidden" name="clicked_x" id="clicked_x" value="no">
-                        <textarea id="post-text" placeholder="Description" id="newpost-content" name="text" rows="10" cols="30" maxlength="256" autofocus></textarea>
+                        <input type="text" id="post-title" placeholder="Group Name" name="country_title" maxlength="64" autofocus>
+                        <select name="group_id" id="group_id" required>
+                                @foreach ($parents as $group)
+                                <option value="{{ $group->id }}">{{ $group->country->name }}</option>
+                                @endforeach
+                        </select>
+                        <textarea id="post-text" placeholder="Description" id="newpost-content" name="text" rows="10" cols="30" maxlength="256" autofocus required></textarea>
                         <button class="button" type="submit">Propose Group</button>
                         </form>
                 </section>
